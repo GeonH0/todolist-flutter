1ec71b8848cdd3a2ab4e7dca6780f64c5389dccf
GeonH0 <h2jo369@naver.com>
2025-06-02 21:45:25 +0900

    Feat: 요일,달을 변환하는 util 추가

--- todolist/lib/utils/date_time_utils.dart
+++ todolist/lib/utils/date_time_utils.dart
@@ -30,3 +30,38 @@ String monthName(int month) {
   ];
   return names[month];
 }
+
+/// 월 숫자를 “Jan”, “Feb” ... 형태로 반환하는 헬퍼
+String monthShortName(int month) {
+  const names = [
+    '',
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec',
+  ];
+  return names[month];
+}
+
+/// 요일 숫자(1=월요일, …, 7=일요일)를 “Mon”, “Tue” ... 형태로 반환
+String weekdayShortName(int weekday) {
+  const names = [
+    '',
+    'Mon',
+    'Tue',
+    'Wed',
+    'Thu',
+    'Fri',
+    'Sat',
+    'Sun',
+  ];
+  return names[weekday];
+}
